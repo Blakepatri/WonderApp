fbb9c01f6151cd27d384313a9da571c9b7787e9e
Paul Gothreau <paulgothreau@gmail.com>
2019-07-26 20:18:24 -0300

    merge

--- sequelize.js
+++ sequelize.js
@@ -15,6 +15,65 @@ const sequelize = new Sequelize('store', 'root', 'password', {
   },
 })
 
+const xinc = new Sequelize('xinc', 'xinc', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  },
+})
+
+const yinc = new Sequelize('yinc', 'yinc', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  },
+})
+
+const zinc = new Sequelize('zinc', 'zinc', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  },
+})
+const InventoryXinc = InventoryModel(xinc, Sequelize)
+const PartXinc = PartModel(xinc, Sequelize)
+const StockLevelXinc = StockLevelModel(xinc, Sequelize)
+const WarehouseXinc = WarehouseModel(xinc, Sequelize)
+
+const InventoryYinc = InventoryModel(yinc, Sequelize)
+const PartYinc = PartModel(yinc, Sequelize)
+const StockLevelYinc = StockLevelModel(yinc, Sequelize)
+const WarehouseYinc = WarehouseModel(yinc, Sequelize)
+
+const InventoryZinc = InventoryModel(zinc, Sequelize)
+const PartZinc = PartModel(zinc, Sequelize)
+const StockLevelZinc = StockLevelModel(zinc, Sequelize)
+const WarehouseZinc = WarehouseModel(zinc, Sequelize)
+
 const force = false //Used to wipe db on start
 
 sequelize.sync({force}).then(() => {
